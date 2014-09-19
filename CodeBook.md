8fdb1074e37bb5d359673bc4665af244b83feadc
Florian Knaus <florian.knaus@gmail.com>
2014-09-19 14:45:44 -0500

    Initial commit

---
+++ CodeBook.md
@@ -0,0 +1,4 @@
+Code Book
+==========
+
+This codebook describes the variables, the data, and any transformations or work that is performed to clean up the data.
---
+++ run_analysis.R
@@ -0,0 +1,12 @@
+##
+## Coursera Data Science Specialization
+## Getting and Cleaning Data Assignment
+## Created by Florian Knaus 9/19/14
+##
+setwd("C:/Users/10131433/Dropbox/eLearning/Data Science/3 Getting and Cleaning Data/Assignment")
+
+# 1 Merge the training and test sets to create one data set.
+# 2 Extract only the measurements on the mean and standard deviation for each measurement
+# 3 Use descriptive activity names to name the activities in the data set
+# 4 Appropriately label the data set with descriptive variable names
+# 5 From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
